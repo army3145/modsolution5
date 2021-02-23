79ab713364d4c520c998a166eb3be0015e42dc6e
army3145 <76448948+army3145@users.noreply.github.com>
2021-02-22 21:56:21 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
 
-      var chosenCategoryShortName = "'" + chooseCategoryShortName(categories).short_name + "'";
+     
        var homeHtmlToInsertIntoMainPage = insert(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
